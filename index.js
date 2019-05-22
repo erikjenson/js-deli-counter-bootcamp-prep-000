b92dcc6bd24f9eb05386f67d0a045fa69b54d9ca
Erik Jenson <erik.a.jenson@gmail.com>
2019-05-22 13:59:17 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -19,10 +19,6 @@ function currentLine(line){
      for (var i=0; i < line.length; i++){
       return `The line is currently: ${i+1}. ${line[i]},`
      }
-     
-     make a loop that cycles through the array listing an index +1 and then the name until for all in line.
-     
-      
   }
  
 }
